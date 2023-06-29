805154e46d323680630d248b4910eb28d9a2d6a0
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-06-29 11:11:33 +0800

    create full usage calculator

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:calculator/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -23,6 +24,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var userQuestion = "";
+  var userAnswer = "";
+
   final List<String> buttons = [
     "C",
     "DEL",
@@ -52,7 +56,31 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.deepPurple[100],
       body: Column(children: <Widget>[
         Expanded(
-          child: Container(),
+          child: Container(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      userQuestion,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      userAnswer,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ]),
+          ),
         ),
         Expanded(
           flex: 2,
@@ -64,18 +92,46 @@ class _HomePageState extends State<HomePage> {
                   itemBuilder: (BuildContext context, int index) {
                     if (index == 0) {
                       return MyButton(
+                        buttonTapped: () {
+                          setState(() {
+                            userQuestion = "";
+                            userAnswer = "";
+                          });
+                        },
                         buttonText: buttons[index],
                         color: Colors.green,
                         textColor: Colors.white,
                       );
                     } else if (index == 1) {
                       return MyButton(
+                        buttonTapped: () {
+                          setState(() {
+                            userQuestion = userQuestion.substring(
+                                0, userQuestion.length - 1);
+                          });
+                        },
                         buttonText: buttons[index],
                         color: Colors.red,
                         textColor: Colors.white,
                       );
+                    } else if (index == buttons.length - 1) {
+                      return MyButton(
+                        buttonTapped: () {
+                          setState(() {
+                            equalPressed();
+                          });
+                        },
+                        buttonText: buttons[index],
+                        textColor: Colors.white,
+                        color: Colors.deepPurple,
+                      );
                     } else {
                       return MyButton(
+                        buttonTapped: () {
+                          setState(() {
+                            userQuestion += buttons[index];
+                          });
+                        },
                         buttonText: buttons[index],
                         color: isOperator(buttons[index])
                             ? Colors.deepPurple
@@ -89,6 +145,17 @@ class _HomePageState extends State<HomePage> {
         )
       ]),
     );
+  }
+
+  void equalPressed() {
+    String finalQuestion = userQuestion;
+    finalQuestion = finalQuestion.replaceAll("x", "*");
+
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    userAnswer = eval.toString();
   }
 
   bool isOperator(String x) {
