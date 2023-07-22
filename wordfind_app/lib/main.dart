3ddbc8fc914eb349fcceed7c069b3be3b6d32aaa
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-07-22 18:12:57 +0800

    introduction page

--- wordfind_app/lib/main.dart
+++ wordfind_app/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
+      theme: ThemeData(fontFamily: "Ribeye"),
       home: Scaffold(
         body: Center(
           // child: WelcomePage(),
--- wordfind_app/lib/widgets/intro.dart
+++ wordfind_app/lib/widgets/intro.dart
@@ -6,11 +6,15 @@ class IntroductionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: Container(
+        child: const Icon(
+          Icons.arrow_back_outlined,
+          color: Colors.white,
+        ),
         width: 32,
         height: 32,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
-
+          color: Color(0xFFFA9541),
         ),
       ),
       backgroundColor: Color(0x4cfbb478),
@@ -43,12 +47,29 @@ class IntroductionPage extends StatelessWidget {
                     fontSize: 14.4,
                     color: Color(0xFFE48000)),
               ),
-
+              Image(image: AssetImage("assets/head.png")),
+              Text(
+                "Player name",
+                style: TextStyle(color: Color(0xFFFA9541), fontSize: 20),
+              ),
+              const SizedBox(
+                width: 250,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Type here",
+                    labelStyle: TextStyle(
+                      fontFamily: "Nunito",
+                      color: Color(0xFFFA9541),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
-
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
     );
   }
 }
