da8b4c835c12e1c5c31e8ba48a3e79330dc03eb5
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-07-24 15:47:03 +0800

    test instagram

---
+++ test_stagram/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+        home: SafeArea(
+          child: Scaffold(
+            body: Container(
+              child: Center(
+                child: Column(
+                  children: [
+
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+  }
+
+
+}
+
+
