1ef737aaeff730a63ca1401ef804fcb6fb3af523
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-07-25 18:17:56 +0800

    modified

--- test_stagram/lib/home_page.dart
+++ test_stagram/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:overscroll_pop/overscroll_pop.dart';
 
 class HomePage extends StatelessWidget {
   final countOfLike;
---
+++ test_stagram/lib/scroll_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:test_stagram/home_page.dart';
+
+
+class ScrollTest extends StatelessWidget {
+  const ScrollTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+      return ListView.custom(childrenDelegate: )
+  }
+  
+}
